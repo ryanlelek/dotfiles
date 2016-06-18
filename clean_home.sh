5fc58c67e0f4876ea3116975b75c7df851c4ba94
Ryan Lelek <ryanlelek@gmail.com>
2016-06-18 19:35:58 -0400

    Script: Removing some annoying junk from home directory

---
+++ clean_home.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm -rf ~/.AndroidStudio1.2/
+rm -rf ~/.AndroidStudio1.5/
+rm -rf ~/.CFUserTextEncoding
+rm -rf ~/.dbshell
+rm -rf ~/.DS_Store
+rm -rf ~/.IdeaIC14
+rm -rf ~/.IdeaIC15
+rm -rf ~/.android
+rm -rf ~/.bash_history
+rm -rf ~/.erlang.cookie
+rm -rf ~/.jssc; #libjssc-java
+rm -rf ~/.node-gyp
+rm -rf ~/.node_repl_history
+rm -rf ~/.oracle_jre_usage
+rm -rf ~/.pm2
+rm -rf ~/.rediscli_history
+rm -rf ~/.sh_history
+rm -rf ~/.v8flags*.json
+rm -rf ~/.viminfo
+rm -rf ~/.vscode
+rm -rf ~/.wget-hsts
+
